17e58dc9159d6dbb6c737db8eb67be088ebea215
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-10-24 16:13:18 -0300

    chore: Update SponsoredBanner component styles

--- react/components/SponsoredBanner/SponsoredBanner.tsx
+++ react/components/SponsoredBanner/SponsoredBanner.tsx
@@ -3,35 +3,47 @@ import { useIntl } from 'react-intl'
 import { useQuery } from 'react-apollo'
 import { useDevice } from 'vtex.device-detector'
 import { Spinner } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
 import sponsoredBannersQuery from 'vtex.store-resources/QuerySponsoredBanners'
 
 import type { SponsoredBannersData } from '../../interfaces'
 import messages from '../../messages'
 
+import './styles.css'
+
+const CSS_HANDLES = ['bannerWrapper', 'bannerButton', 'bannerImage'] as const
+
 export const SponsoredBanner = () => {
+  const handles = useCssHandles(CSS_HANDLES)
   const { formatMessage } = useIntl()
-  const { isMobile } = useDevice()
+  const { device } = useDevice()
+  const isDesktop = device === 'desktop'
+
   const { data, loading } = useQuery<SponsoredBannersData>(
     sponsoredBannersQuery,
     {
       variables: {
-        channel: isMobile ? 'mobile' : 'website',
+        placement: 'homepage',
+        adUnit: isDesktop ? 'billboard' : 'smartphone-banner',
+        channel: isDesktop ? 'website' : 'mobile',
       },
     }
   )
 
   const styleProps = {
-    width: isMobile ? '320px' : '1232px',
-    height: isMobile ? '50px' : '270px',
-    spinnerSize: isMobile ? 20 : 40,
+    width: isDesktop ? '1232px' : '320px',
+    height: isDesktop ? '270px' : '50px',
+    spinnerSize: isDesktop ? 40 : 20,
+  }
+
+  function handleClick(url: string) {
+    window.open(url, '_self')
   }
 
   return (
     <div
+      className={`${handles.bannerWrapper} flex justify-center items-center`}
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         width: styleProps.width,
         height: styleProps.height,
       }}
@@ -40,15 +52,23 @@ export const SponsoredBanner = () => {
         <Spinner size={styleProps.spinnerSize} />
       ) : (
         data?.sponsoredBanners.map((banner) => (
-          <img
+          <button
+            className={handles.bannerButton}
             key={banner.bannerId}
-            src={banner.advertisement.imageUrl}
-            alt={formatMessage(messages.sponsoredBanner)}
-            style={{
-              width: styleProps.width,
-              height: styleProps.height,
+            onClick={() => {
+              handleClick(banner.advertisement.targetUrl)
             }}
-          />
+          >
+            <img
+              className={handles.bannerImage}
+              src={banner.advertisement.imageUrl}
+              alt={formatMessage(messages.sponsoredBanner)}
+              style={{
+                width: styleProps.width,
+                height: styleProps.height,
+              }}
+            />
+          </button>
         ))
       )}
     </div>
