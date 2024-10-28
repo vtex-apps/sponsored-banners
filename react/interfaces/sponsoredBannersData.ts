6e7e6f8a18967c1f1c0ab2e635a98b712b9436f6
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-10-28 16:17:30 -0300

    chore: update SponsoredBannersData response

--- react/interfaces/sponsoredBannersData.ts
+++ react/interfaces/sponsoredBannersData.ts
@@ -1,16 +1,17 @@
 export interface Advertisement {
-  actionCost: number
-  adRequestId: string
   adResponseId: string
-  campaignId: string
   imageUrl: string
   targetUrl: string
   bannerImageId: string
+  width: number
+  height: number
 }
+
 interface SponsoredBanner {
   bannerId: string
   advertisement: Advertisement
 }
+
 export interface SponsoredBannersData {
   sponsoredBanners: SponsoredBanner[]
 }
