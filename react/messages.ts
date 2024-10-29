2cb6dac884889debe04cd9d253ea3dfe1d403529
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2024-10-29 14:31:07 -0300

    chore: Update language translations for admin/editor components

--- react/messages.ts
+++ react/messages.ts
@@ -4,7 +4,13 @@ const messages = defineMessages({
   sponsoredBanner: { id: 'store/sponsored-banner' },
   editorAdUnit: { id: 'admin/editor.ad-unit' },
   editorPlacement: { id: 'admin/editor.placement' },
+  editorPlacementHomepage: { id: 'admin/editor.placement.homepage' },
   editorChannel: { id: 'admin/editor.channel' },
+  editorChannelWebsite: { id: 'admin/editor.channel.website' },
+  editorChannelAndroid: { id: 'admin/editor.channel.android' },
+  editorChannelIos: { id: 'admin/editor.channel.ios' },
+  editorChannelMsite: { id: 'admin/editor.channel.msite' },
+  editorChannelWhatsapp: { id: 'admin/editor.channel.whatsapp' },
 })
 
 export default messages
